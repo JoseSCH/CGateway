80583b6536ac63e4cb48081626c2d245a1b30b32
chepe <jsozachevez@gmail.com>
2022-11-21 19:09:04 -0600

    Tratando de que funcione

--- cg_header.php
+++ cg_header.php
@@ -13,36 +13,34 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 //seguridad
 defined('ABSPATH') or die('No se admiten trampas');
 
-//Gancho de tipo filtro para registrar nuestra clase php como una pasarela de pago ante woocommerce_payment_gateways
-add_filter( 'woocommerce_payment_gateways', 'CG_class_agregar' );
-function CG_class_agregar( $gateways ) {
-	$gateways[] = 'CG_Payment_class'; // El nombre de la clase.
+add_filter( 'woocommerce_payment_gateways', 'CG_Agregar_Pasarela' );
+function CG_Agregar_Pasarela( $gateways ) {
+	$gateways[] = 'CG_CHEPE_Gateway';
 	return $gateways;
 }
 
-//La clase, esta dentro del gancho de acción 'plugins loaded'
-add_action('plugins_loaded', "Iniciar_Clase_GC");
-function Iniciar_Clase_GC(){
+add_action( 'plugins_loaded', 'CG_init_pasarela_class' );
+function CG_init_pasarela_class() {
 
-    class CG_Payment_class extends WC_Payment_Gateway{
+    class CG_CHEPE_Gateway extends WC_Payment_Gateway {
 
-        //constructor
-        public function __construct(){
+        public function __construct() {
 
-            $this->id = 'CHEPE';//ID de la pasarela.
-            $this->icon = '';//URL de icono si se usa o nel.
-            $this->has_fields = true;//Para aceptar formularios personalizados.
-            $this->method_title = 'CG Payment Gateway';//Título del metodo.
-            $this->method_description = 'Yo solo se que no se nada';//Descripción del método.
-
-            $this->supporst = array(//Para pagos simples.
+            $this->id = 'chepe';
+            $this->icon = ''; 
+            $this->has_fields = true;
+            $this->method_title = 'Pasarela de pago CG';
+            $this->method_description = 'Bienvenido a esta pequeña y amiga pasarela';
+        
+           
+            $this->supports = array(
                 'products'
             );
-
-            //Metodo con todos los campos.
+        
+            
             $this->init_form_fields();
+        
 
-            //Cargar opciones.
             $this->init_settings();
             $this->title = $this->get_option( 'title' );
             $this->description = $this->get_option( 'description' );
@@ -50,13 +48,13 @@ function Iniciar_Clase_GC(){
             $this->testmode = 'yes' === $this->get_option( 'testmode' );
             $this->private_key = $this->testmode ? $this->get_option( 'test_private_key' ) : $this->get_option( 'private_key' );
             $this->publishable_key = $this->testmode ? $this->get_option( 'test_publishable_key' ) : $this->get_option( 'publishable_key' );
-
-            //Gancho de acción que guarda las opciones.
+        
+          
             add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-
-            //Para obtener el token desde JavaScript.
+        
+           
             add_action( 'wp_enqueue_scripts', array( $this, 'payment_scripts' ) );
-
+            
         }
 
         public function init_form_fields(){
@@ -64,7 +62,7 @@ function Iniciar_Clase_GC(){
             $this->form_fields = array(
                 'enabled' => array(
                     'title'       => 'Enable/Disable',
-                    'label'       => 'Enable CG Gateway',
+                    'label'       => 'Enable Misha Gateway',
                     'type'        => 'checkbox',
                     'description' => '',
                     'default'     => 'no'
@@ -72,21 +70,21 @@ function Iniciar_Clase_GC(){
                 'title' => array(
                     'title'       => 'Title',
                     'type'        => 'text',
-                    'description' => 'This controls the title which the user sees during checkout.',
-                    'default'     => 'Credit Card',
+                    'description' => 'Controla el título de lo que el usuario ve al momento de pagar.',
+                    'default'     => 'Credit Card - Por Pasarela CG',
                     'desc_tip'    => true,
                 ),
                 'description' => array(
                     'title'       => 'Description',
                     'type'        => 'textarea',
-                    'description' => 'This controls the description which the user sees during checkout.',
-                    'default'     => 'Pay with your credit card via our super-cool payment gateway.',
+                    'description' => 'La descripción que el cliente mira al momento de pagar',
+                    'default'     => 'Paga con tu tarjeta de crédito de una manera tuani.',
                 ),
                 'testmode' => array(
                     'title'       => 'Test mode',
                     'label'       => 'Enable Test Mode',
                     'type'        => 'checkbox',
-                    'description' => 'Place the payment gateway in test mode using test API keys.',
+                    'description' => 'Pone al servicio el modo de prueba con sus respectivas API keys.',
                     'default'     => 'yes',
                     'desc_tip'    => true,
                 ),
@@ -107,43 +105,135 @@ function Iniciar_Clase_GC(){
                     'type'        => 'password'
                 )
             );
-
         }
 
         public function payment_scripts() {
 
-            // we need JavaScript to process a token only on cart/checkout pages, right?
+
             if ( ! is_cart() && ! is_checkout() && ! isset( $_GET['pay_for_order'] ) ) {
                 return;
             }
         
-            // if our payment gateway is disabled, we do not have to enqueue JS too
+ 
             if ( 'no' === $this->enabled ) {
                 return;
             }
         
-            // no reason to enqueue JavaScript if API keys are not set
+ 
             if ( empty( $this->private_key ) || empty( $this->publishable_key ) ) {
                 return;
             }
         
-            // do not work with card detailes without SSL unless your website is in a test mode
+           
             if ( ! $this->testmode && ! is_ssl() ) {
                 return;
             }
         
-            // let's suppose it is our payment processor JavaScript that allows to obtain a token
+           
             wp_enqueue_script( 'rigel_js', 'https://www.mishapayments.com/api/token.js' );
         
-            // and this is our custom JS in your plugin directory that works with token.js
+           
             wp_register_script( 'woocommerce_misha', plugins_url( 'rigel.js', __FILE__ ), array( 'jquery', 'rigel_js' ) );
         
-            // in most payment processors you have to use PUBLIC KEY to obtain a token
-            wp_localize_script( 'woocommerce_misha', 'misha_params', array(
+        
+            wp_localize_script( 'woocommerce_chepe', 'chepe_params', array(
                 'publishableKey' => $this->publishable_key
             ) );
         
             wp_enqueue_script( 'woocommerce_chepe' );
+        
+        }
+
+        public function payment_fields() {
+ 
+
+            if ( $this->description ) {
+   
+                if ( $this->testmode ) {
+                    $this->description .= 'El modo de prueba esta activado';
+                    $this->description  = trim( $this->description );
+                }
+  
+                echo wpautop( wp_kses_post( $this->description ) );
+            }
+         
+            
+            echo '<fieldset id="wc-' . esc_attr( $this->id ) . '-cc-form" class="wc-credit-card-form wc-payment-form" style="background:transparent;">';
+         
+           
+            do_action( 'woocommerce_credit_card_form_start', $this->id );
+         
+           
+            echo '<div class="form-row form-row-wide"><label>Card Number <span class="required">*</span></label>
+                <input id="misha_ccNo" type="text" autocomplete="off">
+                </div>
+                <div class="form-row form-row-first">
+                    <label>Expiry Date <span class="required">*</span></label>
+                    <input id="misha_expdate" type="text" autocomplete="off" placeholder="MM / YY">
+                </div>
+                <div class="form-row form-row-last">
+                    <label>Card Code (CVC) <span class="required">*</span></label>
+                    <input id="misha_cvv" type="password" autocomplete="off" placeholder="CVC">
+                </div>
+                <div class="clear"></div>';
+         
+            do_action( 'woocommerce_credit_card_form_end', $this->id );
+         
+            echo '<div class="clear"></div></fieldset>';
+         
+        }
+
+        public function validate_fields(){
+ 
+            if( empty( $_POST[ 'billing_first_name' ]) ) {
+                wc_add_notice(  'First name is required!', 'error' );
+                return false;
+            }
+            return true;
+         
+        }
+
+        public function process_payment( $order_id ) {
+ 
+            global $woocommerce;
+         
+           
+            $order = wc_get_order( $order_id );
+
+            $response = wp_remote_post( '{https://eureka.free.beeceptor.com}', $args );
+
+            if( !is_wp_error( $response ) ) {
+ 
+                $body = json_decode( $response['body'], true );
+        
+                
+                if ( $body['response']['responseCode'] == 'APPROVED' ) {
+        
+                  
+                   $order->payment_complete();
+                   $order->reduce_order_stock();
+        
+                
+                   $order->add_order_note( 'Hey, your order is paid! Thank you!', true );
+        
+                
+                   $woocommerce->cart->empty_cart();
+        
+           
+                   return array(
+                       'result' => 'success',
+                       'redirect' => $this->get_return_url( $order )
+                   );
+        
+                } else {
+                   wc_add_notice(  'Please try again.', 'error' );
+                   return;
+               }
+        
+           } else {
+               wc_add_notice(  'Connection error.', 'error' );
+               return;
+           }
         
         }
 
